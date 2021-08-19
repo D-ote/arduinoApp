28ab3042da7d2d6244301728fcd8f3824d25546e
d-ote <dooterior@gmail.com>
2021-08-18 20:28:31 -0700

    edited lists page

--- lists/listsPages.js
+++ lists/listsPages.js
@@ -1,6 +1,8 @@
 const smsModal = document.querySelector(".smsModal");
 const smsDiv = document.querySelector(".smsDiv");
 const errorDiv = document.querySelector(".errorDiv");
+const navItem1 = document.querySelector(".navItem1")
+const navItem2 = document.querySelector(".navItem2")
 
 const displaySMSDetails = () => {
     if (smsModal.style.display === "none") {
@@ -21,15 +23,19 @@ const closeModal = () => {
 const displaySMSList = () => {
     if (smsDiv.style.display === "none") {
         smsDiv.style.display = "block"
+        navItem2.classList.remove("clicked")
+        navItem1.classList.add("clicked")
         errorDiv.style.display = "none"
     } else {
-        smsDiv.style.display = "none"
+        navItem1.classList.add("clicked")
     }
 }
 
 const displayErrorList = () => {
     if (errorDiv.style.display === "none") {
         errorDiv.style.display = "block"
+        navItem1.classList.remove("clicked")
+        navItem2.classList.add("clicked")
         smsDiv.style.display = "none"
     } else {
         errorDiv.style.display = "none"
